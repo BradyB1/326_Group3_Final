1df5b08e4a2b010b7b0f18ff3e3d33374306ed55
BradyB1 <117840393+BradyB1@users.noreply.github.com>
2022-12-06 16:03:19 -0500

    Add files via upload

--- Project.py
+++ Project.py
@@ -3,15 +3,48 @@
 # INST 326: Final Project
 # 12/15/2022
 
+# for ease of copy paste
+#UserCompany,UserTypeName,UserInches, UserResolution, UserCpu, UserRam, UserMemory, UserGpu, UserOpSys, UserWeight, UserPrice
+
 import sys
 import argparse
 import pandas as pd
 import re
 import unittest
 '''
-Using this software, users can input their desired laptop components and if two of more of the components match from an individual laptop, the laptop name information will be returned for the user.
+Using this software, users can input their desired laptop components and if two of more of the components match from an individual laptop, the laptop ID# along with the matching specs will be returned for the user.
 
 '''
+# NEED 1 class and 8 methods
+
+
+def SearchOption():
+    searchBy = input(
+        "Would you like to search by specs or by price? (Please enter 'specs' or 'price)\n")
+
+    if searchBy not in ["Specs", "specs", "Price", "price"]:
+        raise ValueError("You must enter a valid response")
+    elif searchBy == "specs" or "Specs":
+        userDesires()
+    elif searchBy == 'price' or "Price":
+        decending_price_display()
+
+
+class Laptop():
+    '''this class will be the laptop objects created from matching laptops'''
+
+    def __init__(self, company, typeName, inches, resolution, cpu, ram, memory, gpu, opSys, weight, price):
+        self.company = company
+        self.typeName = typeName
+        self.inches = inches
+        self.resolution = resolution
+        self.cpu = cpu
+        self.ram = ram
+        self.memory = memory
+        self.gpu = gpu
+        self.opSys = opSys
+        self.weight = weight
+        self.price = price
 
 
 def userDesires():
@@ -32,7 +65,10 @@ Args:
 returns: userInput
 
     '''
+
+    # This list will hold the values of the user input
     specs = []
+
     UserCompany = input(
         "What company are you searching for? (if none say 'NA')")
 
@@ -58,35 +94,85 @@ returns: userInput
 
     UserWeight = input("What Weight are you looking for? (if none say 'NA')")
 
-    UserPrice = input("What Price are you looking around? (if none say 'NA')")
+    dollarAmount = input(
+        "What Price are you looking around? (if none say 'NA')")
+    # make this its own conversion function
+    # add budget/price range instead of hard price
+    # display it from high to low
+    # conversion from USD to INR
+
     # create array and append these to it
 
-    print(f"Company desired: {UserCompany}")
-    # if UserCompany != "NA" or 'na':
-    #     specs.append(UserCompany)
-    print(f"Type: {UserTypeName}")
-    specs.append(UserTypeName)
-    print(f"Inches: {UserInches}")
-    specs.append(UserInches)
-    print(f"Screen Resolution: {UserResolution}")
-    specs.append(UserResolution)
-    print(f"Cpu: {UserCpu}")
-    specs.append(UserCpu)
-    print(f"Ram: {UserRam}")
-    specs.append(UserRam)
-    print(f"Storage Memory: {UserMemory}")
-    specs.append(UserMemory)
-    print(f"Gpu: {UserGpu}")
-    specs.append(UserGpu)
-    print(f"Operating system: {UserOpSys}")
-    specs.append(UserOpSys)
-    print(f"Laptop Weight: {UserWeight}")
-    specs.append(UserWeight)
-    print(f"Price Range: {UserPrice}")
+    # Series of if/else statements to check if the value is equal to something other then NA.
+    # if NA pop it off the list
+    # else append it to the list
+    if UserCompany not in ["NA", "na", 'None', 'none']:
+        specs.append(UserCompany)
+
+    if UserTypeName not in ["NA", "na", 'None', 'none']:
+        specs.append(UserTypeName)
+
+    if UserInches not in ["NA", "na", 'None', 'none']:
+        specs.append(UserInches)
+
+    if UserResolution not in ["NA", "na", 'None', 'none']:
+        specs.append(UserResolution)
+
+    if UserCpu not in ["NA", "na", 'None', 'none']:
+        specs.append(UserCpu)
+
+    if UserRam not in ["NA", "na", 'None', 'none']:
+        specs.append(UserRam)
+
+    if UserMemory not in ["NA", "na", 'None', 'none']:
+        specs.append(UserMemory)
+
+    if UserGpu not in ["NA", "na", 'None', 'none']:
+        specs.append(UserGpu)
+
+    if UserOpSys not in ["NA", "na", 'None', 'none']:
+        specs.append(UserOpSys)
+
+    if UserWeight not in ["NA", "na", 'None', 'none']:
+        specs.append(UserWeight)
+
+    return specs
+
+    # Make this a Userdisplay method
+
+
+def price_conversion(dollarAmount, specs):
+    '''This method converts USD amount to INR amount. (the dataset contains INR prices)'''
+    UserPrice = int(dollarAmount) / .0121
+    print(
+        f"User price in dollars is: {dollarAmount}, user price in Ruppe is: {UserPrice}")
     specs.append(UserPrice)
 
-    print(specs)
-    return specs
+
+def UserDisplay(UserCompany, UserTypeName, UserInches, UserResolution, UserCpu, UserRam, UserMemory, UserGpu, UserOpSys, UserWeight, UserPrice):
+    '''This will display what the user inputs '''
+    print(f"Company desired: {UserCompany}")
+
+    print(f"Type: {UserTypeName}")
+
+    print(f"Inches: {UserInches}")
+
+    print(f"Screen Resolution: {UserResolution}")
+
+    print(f"Cpu: {UserCpu}")
+
+    print(f"Ram: {UserRam}")
+
+    print(f"Storage Memory: {UserMemory}")
+
+    print(f"Gpu: {UserGpu}")
+
+    print(f"Operating system: {UserOpSys}")
+
+    print(f"Laptop Weight: {UserWeight}")
+
+    print(f"Price Range: {UserPrice}")
+
 
 # class TestBot(unittest.TestCase):
 
@@ -101,192 +187,56 @@ def main(filename, laptop_data):
     # df = pd.read_csv(filename)
     print(laptop_data)
 
-    # we can use this to check each component
-    for i in laptop_data:
-        pass
 
-    userDesires()
-
-
-def parse_company(laptop_data):
-    '''This function will parse the csv file to find matching company values 
-
-    '''
-    for row in laptop_data:
-        if "Apple" in laptop_data:
-            print(row, "found")
-
-    pass
-
-
-def test_companyParse():
-    '''This function will test if the parse_company function is working  
-
-    '''
-    pass
-
-
-def parse_typeName():
-    '''this function this will parse the csv file to find matching Type Name values 
-
-    '''
-    pass
-
-
-def test_TypeParse():
-    '''This function will test if the parse_TypeName function is working  
-
-    '''
-    pass
-
-
-def parse_inches():
-    '''this functino will parse the csv file to find matching inches values 
-    '''
-    pass
-
-
-def test_inchesParse():
-    '''This function will test if the parse_inches function is working  
-
-    '''
-    pass
-
-
-def parse_resolution():
-    '''this functino will parse the csv file to find matching resolution values 
-    '''
-    pass
-
-
-def test_resParse():
-    '''This function will test if the parse_resolution function is working  
-
-    '''
-    pass
-
-
-def parse_cpu():
-    '''this functino will parse the csv file to find matching cpu values 
-    '''
-    pass
-
-
-def test_cpuParse():
-    '''This function will test if the parse_cpu function is working  
-
-    '''
-    pass
-
-
-def parse_ram():
-    '''this functino will parse the csv file to find matching RAM values 
-    '''
-    pass
-
-
-def test_ramParse():
-    '''This function will test if the parse_ram function is working  
-
-    '''
-    pass
-
-
-def parse_memory():
-    '''this functino will parse the csv file to find matching memory values 
-    '''
-    pass
-
-
-def test_memoryParse():
-    '''This function will test if the parse_memory function is working  
-
-    '''
-    pass
-
-
-def parse_gpu():
-    '''this functino will parse the csv file to find matching gpu values 
-    '''
-    pass
-
-
-def test_gpuParse():
-    '''This function will test if the parse_gpu function is working  
-
-    '''
-    pass
-
-
-def parse_opSys():
-    '''this functino will parse the csv file to find matching OS values 
-    '''
-    pass
-
-
-def test_opSysParse():
-    '''This function will test if the parse_opSys function is working  
-
-    '''
-    pass
-
-
-def parse_weight():
-    '''this functino will parse the csv file to find matching weight values 
-    '''
-    pass
-
-
-def test_weightParse():
-    '''This function will test if the parse_weight function is working  
-
-    '''
-    pass
-
-
-def parse_price():
-    '''this functino will parse the csv file to find matching price range values 
-    '''
-    pass
-
-
-def test_priceParse():
-    '''This function will test if the parse_price function is working  
-
-    '''
-    pass
-
-
-def matches():
+def matches(specs, laptop_data):
     '''This function will check if at least two or more components are matching the users desires.
 
     returns: matching laptops
             "There are no matching laptops
     '''
+    output = []
+    spec_count = 0
 
-    pass
+    for row in laptop_data:
+        for column in row:
+            if column in specs:
+                spec_count += 1
+    if spec_count >= 2:
+        laptop = Laptop(row[1], row[2], row[3], row[4],
+                        row[5], row[6], row[7], row[8], row[9], row[10], row[11])
+        # ^this is what we need to create laptop class for
+        # output.append(laptop)
+
+        # for row in df:
+        # for col in row:
+        # if value in specs:
+        # spec_count+=1
+        # if spec_count >= 2:
+        # laptop = Laptop(CompanyName... etc)
+        # ^get that from row
+        # output.append(laptop)
+
+        # use info in row to create laptop obj
+
+        # return output
 
 
-def parse_args(args_list):
-    """Takes a list of strings from the command prompt and passes them through as arguments
+# CODE TO SHOW ASCENDING PRICES
 
-    Args:
-        args_list (list) : the list of strings from the command prompt
-    Returns:
-        args (ArgumentParser)
-    """
 
-    parser = argparse.ArgumentParser()
+def decending_price_display(laptop_data):
+    '''This function works to display the prices in decending order (lowest -> highest)'''
+    laptop_data.sort_values(laptop_data.columns[11],
+                            axis=0,
+                            ascending=[False],
+                            inplace=True)
 
-    parser.add_argument('--filename', type=str, help='Name of the file.')
-    # parser.add_argument('--species', type = str, help = 'IM JUST USING THIS AS A HOLDER FOR THE MOMENT.')
-
-    args = parser.parse_args(args_list)
-
-    return args
+    # displaying sorted data frame
+    print(laptop_data)
 
 
 if __name__ == "__main__":
-    parse_company()
+    price_conversion()
+    # userDesires()
     #args = parse_args(sys.argv[1:])
     # main(args.filename)
